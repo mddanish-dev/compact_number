2ea7d88800ef42517657296016c8812f6c2eea78
mddanish-commits <developer@mddanish.dev>
2022-08-15 12:28:14 +0530

    added example

---
+++ example/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:compact_number/compact_number.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Compact Number'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  getCompactNumber(number) {
+    final cn = CompactNumber();
+    String result = cn.getValue(number);
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "This youtube video has " + getCompactNumber(2000000) + " views.",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
